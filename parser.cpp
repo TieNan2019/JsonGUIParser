75467fae08b60b8a430e633edb93a05f448a0c6d
TieNan2019 <cooldownlengque@163.com>
2020-12-23 17:25:36 +0800

    function to view json-format instances

--- parser.cpp
+++ parser.cpp
@@ -16,7 +16,7 @@ QString Parser::FileRead(const QString& path) {
         QString jsonStr;
         QTextStream in(&jsonFile);
         while (!in.atEnd()) {
-            jsonStr += in.readLine();
+                jsonStr += in.readLine();
         }
 
 #ifdef __DEBUG__
@@ -25,7 +25,6 @@ QString Parser::FileRead(const QString& path) {
 
         return jsonStr;
 }
-
 
 
 
@@ -45,19 +44,17 @@ QJsonObject Parser::JsonParse(const QString& jsonStr) {
 
 
 
-
 QJsonObject *Parser::JsonLoader(const QString &path) {
         QString jsonStr = FileRead(path);
 
         this->jsonObj = new QJsonObject(JsonParse(jsonStr));
 
-#ifdef  __DEBUG__
+#ifdef  __DEBUG__onst
         qDebug() << this->jsonObj->take("type");
 #endif
 
         return this->jsonObj;
 }
-
 
 
 
@@ -76,62 +73,233 @@ void Parser::InitWidget() {
 
 
 QComboBox *Parser::ServLoader() {
-        /* use smart pointer to do a judgement before alloack */
-        // if (!this->comboBoxServ)
-        //         qDebug() << "NULL";
-
-        this->comboBoxServ = new QComboBox(this);
-
         QJsonArray services = this->jsonObj->value("services").toArray();
 
-//        for (auto service : services) {
-        for (int i = 0; i < services.size(); i++) {
-
-                QJsonObject item = services[i].toObject();
+        for (auto service : services) {
+                QJsonObject item = service.toObject();
 #ifdef __DEBUG__
                 qDebug() << item["description"].toString()
                 << " : " << item["properties"];
 #endif
 
-                comboBoxServ->addItem(QString::number(i) + " "
-                + item["description"].toString());
+                comboBoxServ->addItem(QString::number(item["iid"].toInt())
+                + "-" + item["description"].toString());
         }
 
-//        comboBoxServ->setCurrentIndex(0);
+        comboBoxServ->setCurrentIndex(2);
 
         return this->comboBoxServ;
 }
 
 
 
+QJsonObject Parser::GetProp() {
+        QString service = this->comboBoxServ->currentText();
+        int servID = service.split('-')[0].toInt();
+
+        QJsonArray props = this->jsonObj->value("services").toArray();
+        for (auto item : props) {
+                QJsonObject prop = item.toObject();
+
+#ifdef __DEBUG__
+                qDebug() << prop["description"] << Qt::endl;
+                qDebug() << prop["iid"] << Qt::endl;
+#endif
+
+                /* iid has to be unique */
+                if (servID == prop["iid"].toInt())
+                        return prop;
+
+        }
+
+        return QJsonObject();
+}
+
 
 QComboBox *Parser::PropLoader() {
-        /* use smart pointer to do a judgement before alloack */
-        // if (!this->comboBoxServ)
-        //         qDebug() << "NULL";
+        QJsonObject props = GetProp();
+        this->comboBoxProp->addItems(props.keys());
 
-        this->comboBoxProp = new QComboBox(this);
-        this->comboBoxProp->move(200, 0);
-
-        QString service = this->comboBoxServ->currentText();
-
-        QJsonArray services = this->jsonObj->value("services").toArray();
-//        services
-
+//        this->comboBoxProp->setCurrentIndex(2);
 
         return this->comboBoxProp;
 }
 
 
+
+QComboBox *Parser::KeyLoader() {
+        if (this->comboBoxIndex->count() == 0)
+                return this->comboBoxKey;
+
+        QJsonObject props = GetProp();
+        QString serv = this->comboBoxProp->currentText();
+
+
+        int iid = this->comboBoxIndex->currentText().split('-')[0].toInt();
+
+        QJsonArray items = props[serv].toArray();
+
+
+        for (auto item : items) {
+                QJsonObject obj = item.toObject();
+                if (obj["iid"].toInt() == iid) {
+                        this->comboBoxKey->addItems(obj.keys());
+
+#ifdef __DEBUG__
+                        qDebug() << obj.keys();
+#endif
+                        return this->comboBoxKey;
+                }
+        }
+
+        return this->comboBoxKey;
+}
+
+
+QPlainTextEdit *Parser::TextLoader() {
+        if (this->comboBoxKey->count() == 0)
+                return this->itemContent;
+
+        QJsonObject props = GetProp();
+        QString serv = this->comboBoxProp->currentText();
+
+        int iid = this->comboBoxIndex->currentText().split('-')[0].toInt();
+
+        QJsonArray items = props[serv].toArray();
+
+        for (auto item : items) {
+                QJsonObject obj = item.toObject();
+                if (obj["iid"].toInt() == iid) {
+
+                        QString key = this->comboBoxKey->currentText();
+                        QJsonValue value = obj[key];
+
+                        if (value.isArray()) {
+                                this->itemContent->setPlainText(QJsonDocument(value.toArray()).toJson());
+                        }
+                        else if (value.isString()) {
+                                this->itemContent->setPlainText(value.toString());
+                        }
+                        else if (value.isDouble()) {
+                                this->itemContent->setPlainText(QString::number(value.toDouble()));
+                        }
+                        else {
+                                QJsonDocument doc = QJsonDocument::fromVariant(value);
+                        }
+                        return this->itemContent;
+                }
+        }
+
+        return this->itemContent;
+}
+
+
+
+QPlainTextEdit *Parser::ShortContent() {
+        QJsonObject prop = GetProp();
+        QString key = this->comboBoxProp->currentText();
+
+#ifdef __DEBUG__
+        qDebug() << prop[key];
+#endif
+
+        if (prop[key].isString()) {
+                this->itemContent->setPlainText(prop[key].toString());
+                this->comboBoxIndex->clear();
+                this->comboBoxKey->clear();
+        }
+        else if (prop[key].isArray()) {
+
+
+                QJsonArray values = prop[key].toArray();
+
+
+                for (auto x : values) {
+                        QJsonObject obj = x.toObject();
+#ifdef __DEBUG__
+                        qDebug() << QString::number(obj["iid"].toInt())
+                        + '-' + obj["description"].toString();
+#endif
+                        this->comboBoxIndex->addItem(
+                        QString::number(obj["iid"].toInt())
+                        + '-' + obj["description"].toString());
+                }
+
+
+        }
+        else if (prop[key].isDouble()) {
+                this->itemContent->setPlainText(QString::number(prop[key].toDouble()));
+                this->comboBoxIndex->clear();
+                this->comboBoxIndex->clear();
+        }
+
+        this->comboBoxIndex->setCurrentIndex(1);
+        KeyLoader();
+
+        return this->itemContent;
+}
+
+
 int Parser::BoxReloader() {
+
+        this->itemContent = new QPlainTextEdit(this);
+        this->itemContent->move(0, 40);
+
+        this->comboBoxKey = new QComboBox(this);
+        this->comboBoxKey->move(600, 0);
+        this->comboBoxKey->resize(180, 30);
+
+        this->comboBoxIndex = new QComboBox(this);
+        this->comboBoxIndex->move(400, 0);
+        this->comboBoxIndex->resize(180, 30);
+
+        this->comboBoxProp = new QComboBox(this);
+        this->comboBoxProp->move(200, 0);
+        this->comboBoxProp->resize(180, 30);
+
+        this->comboBoxServ = new QComboBox(this);
+        this->comboBoxServ->move(0, 0);
+        this->comboBoxServ->resize(180, 30);
 
         this->comboBoxServ = ServLoader();
         this->comboBoxProp = PropLoader();
+        this->ShortContent();
+
+
+        this->comboBoxKey->setCurrentIndex(5);
+
+
+        this->TextLoader();
+
+        connect(this->comboBoxKey, SIGNAL(currentIndexChanged(int)), this, SLOT(KeyChanged(int)));
+        connect(this->comboBoxIndex, SIGNAL(currentIndexChanged(int)), this, SLOT(IndexChanged(int)));
+        connect(this->comboBoxProp, SIGNAL(currentIndexChanged(int)), this, SLOT(PropChanged(int)));
+        connect(this->comboBoxServ, SIGNAL(currentIndexChanged(int)), this, SLOT(ServChanged(int)));
 
         return 0;
 }
 
 
+void Parser::KeyChanged(int idx) {
+        this->TextLoader();
+}
+
+void Parser::IndexChanged(int idx) {
+        this->comboBoxKey->clear();
+        this->KeyLoader();
+}
+
+void Parser::PropChanged(int idx) {
+        this->comboBoxIndex->clear();
+        this->ShortContent();
+}
+
+
+void Parser::ServChanged(int idx) {
+        this->comboBoxProp->clear();
+        this->itemContent->clear();
+        this->PropLoader();
+}
 
 
 Parser::Parser(QWidget *parent)
@@ -140,9 +308,9 @@ Parser::Parser(QWidget *parent)
 {
         this->jsonObj = JsonLoader(INSTANCE_PATH);
 
-        BoxReloader();
-
         InitWidget();
+
+        BoxReloader();
 
         ui->setupUi(this);
 }
@@ -156,6 +324,8 @@ Parser::~Parser()
 
         delete comboBoxServ;
         delete comboBoxProp;
+        delete itemContent;
+        delete comboBoxIndex;
 
         delete ui;
 }
--- parser.h
+++ parser.h
@@ -11,11 +11,13 @@
 #include <QJsonDocument>
 
 
-
-
 #include <QDebug>
-
 //#define __DEBUG__
+
+
+
+#include <QPlainTextEdit>
+
 
 
 #define INSTANCE_PATH   "/home/whoami/workspace/MIoT/lab/massager.json"
@@ -33,18 +35,33 @@ public:
         Parser(QWidget *parent = nullptr);
         ~Parser();
 
+private slots:
+        void KeyChanged(int );
+        void IndexChanged(int );
+        void PropChanged(int );
+        void ServChanged(int );
+
+
 private:
         QJsonObject *jsonObj;
         QComboBox *comboBoxServ;
         QComboBox *comboBoxProp;
+        QComboBox *comboBoxIndex;
+        QComboBox *comboBoxKey;
+        QPlainTextEdit *itemContent;
 
         QString FileRead(const QString&);
         QJsonObject JsonParse(const QString&);
         QJsonObject *JsonLoader(const QString &);
 
         void InitWidget();
+
+        QJsonObject GetProp();
         QComboBox *ServLoader();
         QComboBox *PropLoader();
+        QPlainTextEdit *ShortContent();
+        QComboBox *KeyLoader();
+        QPlainTextEdit *TextLoader();
         int BoxReloader();
 
         Ui::Parser *ui;
